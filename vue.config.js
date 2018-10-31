835233493db41e99e0949b979b8b176805dc2692
Root Lin(office) <root_lin@designreactor.com>
2018-10-31 21:19:41 +0800

    设置项目前缀

--- vue.config.js
+++ vue.config.js
@@ -3,12 +3,10 @@
 // https://github.com/vuejs/vue-cli/tree/dev/docs/config
 
 // https://phoebecodespace.github.io/2018/04/19/vue-cli-3-briefly-analyzes/#%E5%8A%9F%E8%83%BD%E9%85%8D%E7%BD%AE
-
-
 if (process.env.NODE_ENV === 'production') {
   module.exports = {
     // options...
-    baseUrl: '/double1111/',
+    baseUrl: '/double11/',
     outputDir: 'docs',
     productionSourceMap: false,
   }
